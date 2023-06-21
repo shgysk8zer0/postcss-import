f90569d26909634d5358d198c3cd598495d594a1
Chris Zuber <shgysk8zer0@gmail.com>
2023-06-20 20:14:26 -0700

    Fix overly permissive regex
    Resolves #2

--- utils.js
+++ utils.js
@@ -3,7 +3,7 @@ import { readFile } from 'node:fs/promises';
 // @SEE `@import` https://developer.mozilla.org/en-US/docs/Web/CSS/@import
 // @SEE RegEx https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Regular_expressions/Named_capturing_group
 export const URL_REGEX = /url\(["']?[^'")]+['"]?\)/g;
-export const IMPORT_REGEX = /^(?:(?:url\(["']?)|['"])(?<path>[^'")]+)(?:(?:["']?\)|['"]))(?:\s+layer\((?<layer>[A-z-]+)\))?(?:\s+supports\((?<supports>[^)]+)\))?(?:\s+(?<media>[^;]*)?\s*)?;?/;
+export const IMPORT_REGEX = /^(?:(?:url\(["']?)|['"])(?<path>[^'")]+)(?:(?:["']?\)|['"]))(?:\s+layer\((?<layer>[A-Za-z-]+)\))?(?:\s+supports\((?<supports>[^)]+)\))?(?:\s+(?<media>[^;]*)?\s*)?;?/;
 
 export function parseImportRule(str) {
 	const { path, supports, layer, media } = IMPORT_REGEX.exec(str.trim())?.groups ?? {};
